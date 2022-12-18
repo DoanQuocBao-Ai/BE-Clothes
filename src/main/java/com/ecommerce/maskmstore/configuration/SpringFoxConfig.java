510e7b445cab15857de5382c03e913a936846e7e
doanquocbao <doanquocbao4117@gmail.com>
2022-12-18 15:36:58 +0700

    update swagger2

--- src/main/java/com/ecommerce/maskmstore/configuration/SpringFoxConfig.java
+++ src/main/java/com/ecommerce/maskmstore/configuration/SpringFoxConfig.java
@@ -6,7 +6,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
+//http://localhost:2021/swagger-ui/index.html#/
 @Configuration
 public class SpringFoxConfig {
     @Bean
